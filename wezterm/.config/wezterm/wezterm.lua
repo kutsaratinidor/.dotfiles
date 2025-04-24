6a72d9bf65c0539b5ca9ddf22c17b49927711388
cedric <276882+kutsaratinidor@users.noreply.github.com>
2025-04-24 19:06:25 -0400

    Updated gitignore, switched starship,tmux, and wezterm theme.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,65 +1,21 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local config = require("config")
+require("events")
 
-config = wezterm.config_builder()
-
-config = {
-    automatically_reload_config = true,
-    enable_tab_bar = false,
-    window_close_confirmation = "NeverPrompt",
-    window_decorations = "RESIZE", --disable title bar but enable the resizable border
-    font = wezterm.font("MesloLGS Nerd Font Mono"),
-    font_size = 12,
-    default_cursor_style = "BlinkingBar",
-    color_scheme = "Nord (Gogh)",
-    window_padding = {
-        left = 3,
-        right = 3,
-        top = 0,
-        bottom = 0,
-    },
-    window_background_opacity = 0.95,
-    -- from: https://akos.ma/blog/adopting-wezterm/
-    hyperlink_rules = {
-    -- Matches: a URL in parens: (URL)
-    {
-      regex = "\\((\\w+://\\S+)\\)",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Matches: a URL in brackets: [URL]
-    {
-      regex = "\\[(\\w+://\\S+)\\]",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Matches: a URL in curly braces: {URL}
-    {
-      regex = "\\{(\\w+://\\S+)\\}",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Matches: a URL in angle brackets: <URL>
-    {
-      regex = "<(\\w+://\\S+)>",
-      format = "$1",
-      highlight = 1,
-    },
-    -- Then handle URLs not wrapped in brackets
-    {
-      -- Before
-      --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-      --format = '$0',
-      -- After
-      regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-      format = "$1",
-      highlight = 1,
-    },
-    -- implicit mailto link
-    {
-      regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-      format = "mailto:$0",
-    },
-  },
+-- Map short names to actual theme names
+local themes = {
+  nord = "Nord (Gogh)",
+  onedark = "One Dark (Gogh)",
 }
+
+-- Get theme from env
+local success, stdout, stderr = wezterm.run_child_process({
+  os.getenv("SHELL"), "-c", "printenv WEZTERM_THEME"
+})
+local selected_theme = stdout:gsub("%s+", "")
+
+-- Apply theme or fallback
+config.color_scheme = themes[selected_theme] or "nord"
+wezterm.log_info("Using theme: " .. tostring(config.color_scheme))
 
 return config
